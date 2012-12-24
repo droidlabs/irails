0410cfc4f6a043222e819e49d51de6e1adb9dd4b
Iskander Haziev <gvalmon@gmail.com>
2012-12-24 22:51:23 +0400

    ability to change password without password

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -14,10 +14,29 @@ ActiveAdmin.register AdminUser do
     end
   end
 
+  controller do
+    def update
+      @admin_user = AdminUser.find(params[:id])
+      password_changed = params[:admin_user][:password].present?
+
+      successfully_updated = if password_changed
+        @admin_user.update_attributes(params[:admin_user])
+      else
+        @admin_user.update_without_password(params[:admin_user])
+      end
+
+      if successfully_updated
+        redirect_to admin_admin_user_path(@admin_user)
+      else
+        render :edit
+      end
+    end
+  end
+
   form do |f|
     f.inputs "Details" do
       f.input :email
-      f.input :password
+      f.input :password, hint: "Keep blank if you won't change password"
     end
     f.buttons
   end
