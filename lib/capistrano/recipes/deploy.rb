bf96a0cc990b24b22a934ea7b967102fa41376e4
Iskander Haziev <gvalmon@gmail.com>
2013-04-15 05:10:47 +0400

    added unciron deploy scripts

--- lib/capistrano/recipes/deploy.rb
+++ lib/capistrano/recipes/deploy.rb
@@ -3,6 +3,8 @@ set(:server_type, "passenger")
 set(:thin_command) { "#{bundle_cmd} exec thin" }
 set(:thin_config_file) { "#{current_path}/thin.yml" }
 set(:thin_config) { "-C #{thin_config_file}" }
+set(:unicorn_conf) { "#{current_path}/config/unicorn.rb" }
+set(:unicorn_pid) { "#{current_path}/tmp/pids/unicorn.pid" }
 
 namespace :deploy do
   task :start do
@@ -45,5 +47,22 @@ namespace :thin do
 
   task :restart do
     run "cd #{current_path} && #{thin_command} #{thin_config} -O restart"
+  end
+end
+
+namespace :unicorn do
+  task :start do
+    run "cd #{deploy_to}/current && #{bundle_cmd rescue 'bundle'} exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
+  end
+  task :stop do
+    run "if [ -f #{unicorn_pid} ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
+  end
+  task :restart, roles: :app, except: { no_release: true } do
+    run "if [ -f #{unicorn_pid} ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{current_path} && #{bundle_cmd rescue 'bundle'} exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
+  end
+
+  desc "Make symlink for unicorn.rb"
+  task :create_symlink do
+    run "ln -nfs #{shared_path}/config/unicorn.rb #{latest_release}/config/unicorn.rb"
   end
 end
