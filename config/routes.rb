ec2a5a6fd1b17594985ad21a55c11951ac4a59ab
Iskander Haziev <gvalmon@gmail.com>
2013-05-06 04:25:41 +0400

    remove unused code

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Irails::Application.routes.draw do
   ActiveAdmin.routes(self)
-  mount Resque::Server.new, at: "/resque" if defined?(Resque)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users
-
-  match '/stripe_listener' => 'subscription_events#stripe', method: :post
-
-  resource :subscription, only: [:edit, :update] do
-    get :card_details
-  end
 
   root to: 'pages#show', id: 'home'
 end
