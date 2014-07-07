6b03024caeed79f3f3d68de8e9eac882ee1f5fc7
Ilya Gorenburg <ilya.gorenburg@gmail.com>
2014-07-06 15:11:29 -1000

    irails is now blank

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,4 @@
 module ApplicationHelper
-  def conditional_html(options = {}, &block)
-    lang = I18n.locale
-    html_class = options[:class]
-    html = <<-"HTML".gsub( /^\s+/, '' )
-      <!--[if lt IE 7 ]>    <html lang="#{lang}" class="#{lang} #{html_class} ie ie6 no-js"> <![endif]-->
-      <!--[if IE 7 ]>       <html lang="#{lang}" class="#{lang} #{html_class} ie ie7 no-js"> <![endif]-->
-      <!--[if IE 8 ]>       <html lang="#{lang}" class="#{lang} #{html_class} ie ie8 no-js"> <![endif]-->
-      <!--[if IE 9 ]>       <html lang="#{lang}" class="#{lang} #{html_class} ie ie9 no-js"> <![endif]-->
-      <!--[if (gte IE 9)|!(IE)]><!--> <html lang="#{lang}" class="#{lang} #{html_class} no-js"> <!--<![endif]-->
-    HTML
-    html += capture( &block ) << "\n</html>".html_safe if block_given?
-    html.html_safe
-  end
-
   def restfull_action_name
     case controller.action_name.to_sym
     when :create
